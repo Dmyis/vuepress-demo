d4438b2cbd083a3571b5aab1a3e7591901d3a244
dmyis <1514378456@qq.com>
2021-10-12 17:12:09 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果你想要部署到 https://USERNAME.github.io
-git push -f git@github.com:Dmyos/Dmyis.github.io.git master
+git push -f git@github.com:Dmyis/Dmyis.github.io.git master
 
 # 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
 # git push -f git@github.com:Dmyis/<REPO>.git master:gh-pages
